2dbd2520902b6ab7efd78de0c778f0236a820515
jorgencio <jorgencio97@gmail.com>
2025-03-12 21:28:37 -0300

    cambio de db

--- tienda_ortopedica/settings.py
+++ tienda_ortopedica/settings.py
@@ -134,13 +134,17 @@ WSGI_APPLICATION = 'tienda_ortopedica.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+#pip install dj-database-url
+#pip install psycopg2-binary
+
+import dj_database_url
+
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgresql://jorgencio:PSWFHpCU9qBgfawFzXa672aNVGpQ2vnW@dpg-cv8g3ni3esus73dedh10-a.oregon-postgres.render.com/mi_tienda_db')
 }
+
 
 
 # Password validation
